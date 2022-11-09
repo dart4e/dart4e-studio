2e767597bbac81502099691b70e742db52f40e7a
sebthom <sebthom@users.noreply.github.com>
2022-11-09 22:25:24 +0100

    Initial import

---
+++ plugin/src/main/java/org/dart4e/studio/Dart4EStudioPlugin.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 by the Dart4E authors.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.dart4e.studio;
+
+import static net.sf.jstuff.core.validation.NullAnalysisHelper.*;
+
+import org.eclipse.jdt.annotation.Nullable;
+import org.osgi.framework.BundleContext;
+
+import de.sebthom.eclipse.commons.AbstractEclipsePlugin;
+import de.sebthom.eclipse.commons.BundleResources;
+import de.sebthom.eclipse.commons.logging.PluginLogger;
+import de.sebthom.eclipse.commons.logging.StatusFactory;
+import net.sf.jstuff.core.validation.Assert;
+
+/**
+ * @author Sebastian Thomschke
+ */
+public class Dart4EStudioPlugin extends AbstractEclipsePlugin {
+
+   private static @Nullable Dart4EStudioPlugin instance;
+
+   /**
+    * @return the shared instance
+    */
+   public static Dart4EStudioPlugin get() {
+      Assert.notNull(instance, "Default plugin instance is still null.");
+      return asNonNullUnsafe(instance);
+   }
+
+   public static PluginLogger log() {
+      return get().getLogger();
+   }
+
+   public static BundleResources resources() {
+      return get().getBundleResources();
+   }
+
+   public static StatusFactory status() {
+      return get().getStatusFactory();
+   }
+
+   @Override
+   public BundleResources getBundleResources() {
+      var bundleResources = this.bundleResources;
+      if (bundleResources == null) {
+         bundleResources = this.bundleResources = new BundleResources(this, "src/main/resources");
+      }
+      return bundleResources;
+   }
+
+   @Override
+   public void start(final BundleContext context) throws Exception {
+      super.start(context);
+      instance = this;
+
+   }
+
+   @Override
+   public void stop(final BundleContext context) throws Exception {
+      instance = null;
+      super.stop(context);
+   }
+}
